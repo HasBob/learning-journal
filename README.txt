2803cb23945ef5023630b98407bede8aa32dfb60
Bob Jenkins <bob@hasoffers.com>
2016-02-04 23:19:45 -0800

    created Entry model class, added some notes

--- README.txt
+++ README.txt
@@ -8,7 +8,17 @@ Getting Started
 
 - $VENV/bin/python setup.py develop
 
-- $VENV/bin/initialize_learning_journal_db development.ini
+- $VENV/bin/setup_db development.ini
 
 - $VENV/bin/pserve development.ini
 
+'''
+from sqlalchemy import engine_from_config
+engine = engine_from_config(registry.settings, 'sqlalchemy.', echo=True)
+from sqlalchemy.orm import sessionmaker
+Session = sessionmaker(bind=engine)
+session = Session()
+from learning_journal.models import Entry
+session.query(Entry).all()
+
+q1 = session.query(Entry.id, Entry.title, Entry.body, Entry.created, Entry.edited)
--- learning_journal/models.py
+++ learning_journal/models.py
@@ -1,8 +1,11 @@
 from sqlalchemy import (
     Column,
+    DateTime,
     Index,
     Integer,
     Text,
+    Unicode,
+    UnicodeText
     )
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -14,14 +17,31 @@ from sqlalchemy.orm import (
 
 from zope.sqlalchemy import ZopeTransactionExtension
 
+import datetime
+
+def now():
+    return datetime.datetime.now()
+
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
 
-class MyModel(Base):
-    __tablename__ = 'models'
-    id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    value = Column(Integer)
+#class MyModel(Base):
+#    __tablename__ = 'models'
+#    id = Column(Integer, primary_key=True)
+#    name = Column(Text)
+#    value = Column(Integer)
 
-Index('my_index', MyModel.name, unique=True, mysql_length=255)
+# http://docs.sqlalchemy.org/en/rel_0_9/core/metadata.html#sqlalchemy.schema.Column
+
+class Entry(Base):
+    __tablename__ = 'entries'
+    id = Column(Integer, primary_key=True)
+    title = Column(Unicode(255), nullable=False) # maybe? types.Unicode ?
+    body = Column(UnicodeText)
+    created = Column(DateTime, default=now) #DateTime? 
+    edited = Column(DateTime, default=None, onupdate=now)
+
+Index('entries_index', Entry.created)
+
+#Index('my_index', MyModel.name, unique=True, mysql_length=255)
