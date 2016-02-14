369d3669357ee9ce0fafcdf89a25a195b250083e
Bob Jenkins <bob@hasoffers.com>
2016-02-14 15:29:27 -0800

    mosupport editing. mostly stolen from the completed version.

--- learning_journal/models.py
+++ learning_journal/models.py
@@ -19,9 +19,6 @@ from zope.sqlalchemy import ZopeTransactionExtension
 
 import datetime
 
-def now():
-    return datetime.datetime.now()
-
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
@@ -37,20 +34,20 @@ Base = declarative_base()
 class Entry(Base):
     __tablename__ = 'entries'
     id = Column(Integer, primary_key=True)
-    title = Column(Unicode(255), nullable=False) # maybe? types.Unicode ?
-    body = Column(UnicodeText)
-    created = Column(DateTime, default=now) #DateTime? 
-    edited = Column(DateTime, default=now, onupdate=now)
+    title = Column(Unicode(255), unique=True, nullable=False)
+    body = Column(UnicodeText, default=u'')
+    created = Column(DateTime, default=datetime.datetime.utcnow)
+    edited = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
 
     @classmethod
     def all(cls, session=None):
-        if session == None:
+        if session is None:
             session = DBSession
-        return session.query(cls).order_by(cls.created).all()
+        return session.query(cls).order_by(cls.created).all() # still needs to be updated to sort descending
 
     @classmethod
     def by_id(cls, id, session=None):
-        if session == None:
+        if session is None:
             session = DBSession
         return session.query(cls).get(id)
 
--- learning_journal/views.py
+++ learning_journal/views.py
@@ -7,16 +7,55 @@ from .models import (
     DBSession,
     Entry,
     )
+from pyramid.httpexceptions import HTTPNotFound
 
+from pyramid.httpexceptions import HTTPFound
+from .forms import (
+    EntryCreateForm,
+    EntryEditForm)
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    try:
-        one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
-    except DBAPIError:
-        return Response(conn_err_msg, content_type='text/plain', status_int=500)
-    return {'one': one, 'project': 'learning_journal'}
+#@view_config(route_name='home', renderer='templates/mytemplate.pt')
+#def my_view(request):
+#    try:
+#        one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
+#    except DBAPIError:
+#        return Response(conn_err_msg, content_type='text/plain', status_int=500)
+#    return {'one': one, 'project': 'learning_journal'}
 
+@view_config(route_name='home', renderer='templates/list.jinja2')
+def index_page(request):
+    entries = Entry.all()
+    return {'entries': entries}
+
+@view_config(route_name='detail', renderer='templates/detail.jinja2')
+def view(request):
+    this_id = request.matchdict.get('id', -1)
+    entry = Entry.by_id(this_id)
+    if not entry:
+        return HTTPNotFound()
+    return {'entry': entry}
+
+@view_config(route_name='action', match_param='action=create', renderer='templates/edit.jinja2')
+def create(request):
+    entry = Entry()
+    form = EntryCreateForm(request.POST)
+    if request.method == 'POST' and form.validate():
+        form.populate_obj(entry)
+        DBSession.add(entry)
+        return HTTPFound(location=request.route_url('home'))
+    return {'form': form, 'action': request.matchdict.get('action')}
+
+@view_config(route_name='action', match_param='action=edit', renderer='templates/edit.jinja2')
+def update(request):
+    id = int(request.params.get('id', -1))
+    entry = Entry.by_id(id)
+    if not entry:
+        return HTTPNotFound()
+    form = EntryEditForm(request.POST, entry)
+    if request.method == 'POST' and form.validate():
+        form.populate_obj(entry)
+        return HTTPFound(location=request.route_url('detail', id=entry.id))
+    return {'form': form, 'action': request.matchdict.get('action')}
 
 conn_err_msg = """\
 Pyramid is having a problem using your SQL database.  The problem
